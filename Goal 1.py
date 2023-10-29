a582b11297a73ec9a95377cd17ec14e759292118
AshX3301 <70807797+AshX3301@users.noreply.github.com>
2023-10-29 19:15:18 +0530

    Update Goal 1.py

--- Goal 1.py
+++ Goal 1.py
@@ -13,34 +13,46 @@ class TurtleController(Node):
         self.cmd_pub = self.create_publisher(Twist, 'turtle1/cmd_vel', 10)
         self.pose_sub = self.create_subscription(Pose, 'turtle1/pose', self.pose_callback, 10)
         self.goal = Point()
-        self.goal.x = 11.0  
-        self.goal.y = 11.0  
-        self.kp = 1.0  
-        self.ki = 0.0  
-        self.kd = 0.0  
+        self.kp = 1.0
+        self.ki = 0.0
+        self.kd = 0.0
         self.prev_error = 0.0
         self.integral = 0.0
+        self.goal_reached = True
+        self.environment_x_max = 11.0  # maximum X coordinate for the environment
+        self.environment_y_max = 11.0  # maximum Y coordinate for the environment
 
     def pose_callback(self, msg):
-        # error
+        self.get_logger().info(f'Turtle Pose: x={msg.x}, y={msg.y}, theta={msg.theta}')
+
+        if self.goal_reached:
+            self.set_random_goal()
+            self.goal_reached = False
+
         error = math.sqrt((self.goal.x - msg.x) ** 2 + (self.goal.y - msg.y) ** 2)
         
-        # control input using PID
+        # PID
         linear_vel = self.kp * error + self.ki * self.integral + self.kd * (error - self.prev_error)
         
-        # angular velocity
+        # Angular velocity
         angle_to_goal = math.atan2(self.goal.y - msg.y, self.goal.x - msg.x)
         angular_vel = 4 * (angle_to_goal - msg.theta)
         
-        # Publish control input
+        
         cmd_vel = Twist()
         cmd_vel.linear.x = linear_vel
         cmd_vel.angular.z = angular_vel
         self.cmd_pub.publish(cmd_vel)
 
-        # Update integral and previous error
         self.integral += error
         self.prev_error = error
+
+    def set_random_goal(self):
+       
+        self.goal.x = random.uniform(1.0, self.environment_x_max)
+        self.goal.y = random.uniform(1.0, self.environment_y_max)
+        
+        self.get_logger().info(f'Random Goal: x={self.goal.x}, y={self.goal.y}')
 
 def main(args=None):
     rclpy.init(args=args)
